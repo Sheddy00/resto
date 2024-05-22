57b848e4aa6b250cd615913d4c893ce2746c15c2
Sheddy00 <hei.sheddy.2@gmail.com>
2024-05-22 03:56:42 +0300

    initial commit: Connection database

---
+++ src/main/java/hei/school/resto/db/DBConnection.java
@@ -0,0 +1,26 @@
+package hei.school.resto.db;
+
+import hei.school.resto.exeption.DataConnectionException;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    private static final String url = System.getenv("URL");
+    private static final String username = System.getenv("USERNAME");
+    private static final String password = System.getenv("PASSWORD");
+
+    public static Connection getConnection(String url, String username, String password) {
+        try {
+            Class.forName("org.postgresql.Driver");
+            return DriverManager.getConnection(url, username, password);
+        } catch (SQLException | ClassNotFoundException e) {
+            throw new DataConnectionException(e.getMessage());
+        }
+    }
+
+    public static Connection getConnection() {
+        return getConnection(url, username, password);
+    }
+}
---
+++ src/main/java/hei/school/resto/exeption/DataConnectionException.java
@@ -0,0 +1,5 @@
+package hei.school.resto.exeption;
+
+public class DataConnectionException extends RuntimeException{
+    public DataConnectionException(String message) { super((message)); }
+}
